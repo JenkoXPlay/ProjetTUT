c7998484e4c5ddd9f8cedd97b84fe9c1a1a72df4
Maxime <maxime.lefebvre-isc.france@ibm.com>
2019-02-15 22:54:47 +0100

    Correction bug swagger

--- function/competences.php
+++ function/competences.php
@@ -1,6 +1,7 @@
 <?php
+
     // Ajout Compétences
-    function addCompetences($bdd, $competenceDe, $domaine, $competence, $level){
+    function addCompetences($bdd, $competenceDe, $domaine, $competence, $level) {
         $req= $bdd->prepare("INSERT INTO competences (id, competenceDe, domaine, competence, level) VALUES (?,?,?,?,?);");
         $req->execute([ 
             '',
@@ -10,38 +11,45 @@
             $level
          ]);
     }
+
     // Delete All compétence
-    function deleteCompAll($bdd){
+    function deleteCompAll($bdd) {
         $req= $bdd->prepare("DELETE FROM competences;");
         $req->execute();
     }
+
     // Delete compétence by ID
     function deleteCompId($bdd,$idComp) {
         $req = $bdd->prepare("DELETE FROM competences WHERE id='$idComp'");
         $req->execute();
     }
+
     // Delete All compétence by competenceDe
     function deleteAllCompDe($bdd,$competenceDe) {
         $req = $bdd->prepare("DELETE FROM competences WHERE competenceDe='$competenceDe'");
         $req->execute();
     }
+
     // Get All compétence
-    function getAllComp($bdd){
+    function getAllComp($bdd) {
         $req = $bdd->prepare("SELECT * FROM competences");
         $req->execute();
         return $req;
 
     }
+
     // Get compétence by ID
-    function getIdComp($bdd,$idComp){
+    function getIdComp($bdd,$idComp) {
         $req = $bdd->prepare("SELECT * FROM competences WHERE id='$idComp'");
         $req->execute();
         return $req;
     } 
+
     // Get All compétence by competenceDe 
-    function getAllCompDe($bdd,$competencesDe){
+    function getAllCompDe($bdd,$competencesDe) {
         $req = $bdd->prepare("SELECT * FROM competences WHERE competenceDe='$competenceDe'");
         $req->execute();
         return $req;
     } 
+
 ?>
--- sql/creaweb_tut.sql
+++ sql/creaweb_tut.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 31 Janvier 2019 à 09:29
+-- Généré le :  Ven 15 Février 2019 à 21:54
 -- Version du serveur :  5.5.49-log
 -- Version de PHP :  7.0.9
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `avatar` text NOT NULL,
   `privilege` varchar(100) NOT NULL,
   `ban` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `admin`
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `annoncesentreprises` (
   `description` text NOT NULL,
   `typeAnnonce` varchar(150) NOT NULL,
   `remuneration` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -107,7 +107,8 @@ CREATE TABLE IF NOT EXISTS `entreprises` (
 --
 
 INSERT INTO `entreprises` (`id`, `responsable`, `nom`, `description`, `logo`, `but`, `typeEntreprise`, `siret`) VALUES
-(5, 1, 'mlklklmkm', 'lmkmlkmk', 'mlkmlkmlklklmk', 'kmklmkmlk', 'pme', 'lmkmkmlk');
+(5, 1, 'mlklklmkm', 'lmkmlkmk', 'mlkmlkmlklklmk', 'kmklmkmlk', 'pme', 'lmkmkmlk'),
+(6, 4, 'IBM', 'Best Company', 'ljfksdjlksjlfjl', 'lkjlkjlkjl', 'ge', 'lsjkflsdjkfljlkdsf');
 
 -- --------------------------------------------------------
 
@@ -125,7 +126,15 @@ CREATE TABLE IF NOT EXISTS `experiences` (
   `dureeTotal` varchar(100) NOT NULL,
   `typeContrat` varchar(100) NOT NULL,
   `poste` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `experiences`
+--
+
+INSERT INTO `experiences` (`id`, `expDe`, `nomEntreprise`, `ville`, `anneeDebut`, `anneeFin`, `dureeTotal`, `typeContrat`, `poste`) VALUES
+(1, 4, 'IBM', 'Lomme', 2018, 2019, '6 mois', 'Contrat Pro', 'Développeur'),
+(2, 5, 'IUT Lens', 'Lens', 2018, 2019, '10 mois', 'Licence', 'Étudiant');
 
 -- --------------------------------------------------------
 
@@ -179,7 +188,14 @@ CREATE TABLE IF NOT EXISTS `messagerie` (
   `message` text NOT NULL,
   `etat_msg` tinyint(1) NOT NULL,
   `date_msg` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `messagerie`
+--
+
+INSERT INTO `messagerie` (`id`, `sender`, `destinataire`, `message`, `etat_msg`, `date_msg`) VALUES
+(1, 1, 1, 'Test de message', 0, '2019-02-02 22:11:27');
 
 -- --------------------------------------------------------
 
@@ -194,7 +210,48 @@ CREATE TABLE IF NOT EXISTS `reponsesannonces` (
   `entreprise` int(11) NOT NULL,
   `datePostuler` datetime NOT NULL,
   `notif` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `swagger`
+--
+
+CREATE TABLE IF NOT EXISTS `swagger` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `categorie` varchar(255) NOT NULL,
+  `requete` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `swagger`
+--
+
+INSERT INTO `swagger` (`id`, `type`, `categorie`, `requete`, `description`) VALUES
+(1, 'post', 'admin', 'addAdmin($bdd, $pseudo, $password, $privilege)', 'Créer un nouveau compte administrateur.\r\nPrivilège : admin, modo'),
+(2, 'delete', 'admin', 'deleteAllAdmin($bdd)', 'Supprime tous les admins'),
+(3, 'delete', 'admin', 'deleteAdmin($bdd, $idAdmin)', 'Supprimer selon l&#039;id du compte Admin'),
+(4, 'get', 'admin', 'getAdmin($bdd)', 'Affiche tous les comptes admin !'),
+(5, 'get', 'admin', 'getAdminId($bdd, $id)', 'Affiche par id d&#039;admin'),
+(6, 'get', 'admin', 'getAdminPseudo($bdd, $pseudo)', 'Affiche par pseudo !'),
+(7, 'post', 'annoncesentreprises', 'addAnnonceCompany($bdd, $entreprise, $titre, $description, $typeAnnonce, $remuneration)', 'Créer une nouvelle annonce'),
+(8, 'delete', 'annoncesentreprises', 'deleteAllAnnonces($bdd)', 'Supprime toutes les annonces'),
+(9, 'delete', 'annoncesentreprises', 'deleteAnnonceId($bdd, $idAnnonce)', 'Supprime une annonce selon son id'),
+(10, 'delete', 'annoncesentreprises', 'deleteAnnonceCompany($bdd, $idCompany)', 'Supprime en fonction de l&#039;id Entreprise'),
+(11, 'get', 'annoncesentreprises', 'getAnnonce($bdd)', 'Récupère toutes les annonces'),
+(12, 'get', 'annoncesentreprises', 'getAnnonceId($bdd, $idAnnonce)', 'Récupère en fonction de l&#039;id'),
+(13, 'get', 'annoncesentreprises', 'getAnnonceId($bdd, $idCompany)', 'Récupère en fonction de l&#039;id Entreprise'),
+(14, 'get', 'annoncesentreprises', 'getAnnonceType($bdd, $typeAnnonce)', 'Récupère selon le type des annonces'),
+(15, 'post', 'competences', 'addCompetences($bdd, $competenceDe, $domaine, $competence, $level)', 'Ajouter une nouvelle compétence'),
+(16, 'delete', 'competences', 'deleteCompAll($bdd)', 'Supprime toutes les compétences !'),
+(17, 'delete', 'competences', 'deleteCompId($bdd,$idComp)', 'Supprime une compétence selon l&#039;id'),
+(18, 'delete', 'competences', 'deleteAllCompDe($bdd,$competenceDe)', 'Supprime toutes les compétences d&#039;un user'),
+(19, 'get', 'competences', 'getAllComp($bdd)', 'Affiche toutes les compétences'),
+(20, 'get', 'competences', 'getIdComp($bdd,$idComp)', 'Affiche une compétence selon l&#039;id'),
+(21, 'get', 'competences', 'getAllCompDe($bdd,$competencesDe)', 'Affiche les compétences d&#039;un utilisateurs');
 
 -- --------------------------------------------------------
 
@@ -218,14 +275,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `raison_ban` text NOT NULL,
   `admin` tinyint(1) NOT NULL,
   `premium` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`id`, `prenom`, `nom`, `email`, `password`, `type_compte`, `departement`, `description`, `telephone`, `date_creation`, `date_last_connexion`, `avert`, `raison_ban`, `admin`, `premium`) VALUES
-(4, 'Maxime', 'Lefebvre', 'max@test.com', '1234', 'etudiant', 62, 'fdsfdsfs', 0, '2019-01-02 00:00:00', '2019-01-24 00:00:00', '0', 'sdsdqsd', 0, 0);
+(4, 'Maxime', 'Lefebvre', 'max@test.com', '1234', 'etudiant', 62, 'fdsfdsfs', 0, '2019-01-02 00:00:00', '2019-01-24 00:00:00', '0', 'sdsdqsd', 0, 0),
+(5, 'Albert', 'Pinot', 'lebgdu62@gmail.com', 'saucisse', 'etudiant', 0, '', 0, '2019-02-07 13:29:52', '2019-02-07 13:29:52', '0', '', 0, 0),
+(6, 'Juju', 'Col', 'lesuperbgdu62@gmail.com', 'saucisse2', 'pro', 0, '', 0, '2019-02-07 14:08:46', '2019-02-07 14:08:46', '0', '', 0, 0);
 
 --
 -- Index pour les tables exportées
@@ -311,6 +370,14 @@ ALTER TABLE `reponsesannonces`
   ADD KEY `notif` (`notif`);
 
 --
+-- Index pour la table `swagger`
+--
+ALTER TABLE `swagger`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categorie` (`categorie`),
+  ADD KEY `type` (`type`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
@@ -325,7 +392,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `administration`
 --
@@ -335,7 +402,7 @@ ALTER TABLE `administration`
 -- AUTO_INCREMENT pour la table `annoncesentreprises`
 --
 ALTER TABLE `annoncesentreprises`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `competences`
 --
@@ -350,7 +417,7 @@ ALTER TABLE `entreprises`
 -- AUTO_INCREMENT pour la table `experiences`
 --
 ALTER TABLE `experiences`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `galerie_entreprises`
 --
@@ -370,17 +437,22 @@ ALTER TABLE `maintenance`
 -- AUTO_INCREMENT pour la table `messagerie`
 --
 ALTER TABLE `messagerie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `reponsesannonces`
 --
 ALTER TABLE `reponsesannonces`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `swagger`
+--
+ALTER TABLE `swagger`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
