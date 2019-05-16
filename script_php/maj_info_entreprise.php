952c56e421d3d33cde85626ec2e51181545e30f8
Unknown <albert_pinot@ens.univ-artois.fr>
2019-05-16 12:47:11 +0200

    ajout edit entreprise et creation

--- script_php/maj_info_entreprise.php
+++ script_php/maj_info_entreprise.php
@@ -14,19 +14,18 @@
                 if(is_numeric($departement)&&($departement > 0 && $departement <= 95)){
                     /*Import Script upload avatar*/
 
-                        $req2 = $bdd->query("SELECT COUNT(*) AS countid FROM entreprises WHERE responsable'{$dataUser['id']}'"); //(responsable = id user)
-                        $reqfetch = $req2->fetch();
-                        if ($reqfetch != 0) {
+                       
+                        //if ($reqfetch != 0) {
                             // entreprise existe
                             $req = $bdd->exec("UPDATE entreprises SET nom='$nom', description='$description', departement='$departement', but='$but', typeEntreprise='$typeEntreprise', siren='$numSiren', siteweb='$siteWeb' WHERE responsable='{$dataUser['id']}'");
                             echo "<div class='alertSuccess width_50 marginAuto'>Votre entreprise a été mise à jour !<br />Redirection en cours...</div><br />";
                             ?><head><meta http-equiv="refresh" content="2;URL=/editentreprise" /></head><?php
-                        } else {
+                        //} 
                             // entreprise à créer
-                            $req = $bdd->exec("INSERT INTO entreprises SET nom='$nom', description='$description', departement='$departement', but='$but', typeEntreprise='$typeEntreprise', siren='$numSiren', siteweb='$siteWeb' WHERE responsable='{$dataUser['id']}'");
+                            /*$req = $bdd->exec("INSERT INTO entreprises SET nom='$nom', description='$description', departement='$departement', but='$but', typeEntreprise='$typeEntreprise', siren='$numSiren', siteweb='$siteWeb' WHERE responsable='{$dataUser['id']}'");
                             echo "<div class='alertSuccess width_50 marginAuto'>Votre entreprise a été créer !<br />Redirection en cours...</div><br />";
-                            ?><head><meta http-equiv="refresh" content="2;URL=/editentreprise" /></head><?php
-                        }
+                            ?><head><meta http-equiv="refresh" content="2;URL=/editentreprise" /></head><?php*/
+                        
 
                    
                 }else echo "<div class='alertError width_50 marginAuto'>Le département n'est pas valide !</div><br />";
