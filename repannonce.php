122542f108f0dc52825057ee6d0ca46f6eb9b472
Maxime <maximelefebvre1505@gmail.com>
2019-05-24 17:51:39 +0200

    Mise en place d'envoie de mail reponse à une candidature

--- repannonce.php
+++ repannonce.php
@@ -39,6 +39,81 @@
                                             <h3>Candidatures de <?php echo $dataAnnonce['titre']; ?></h3><br />
 
                                             <?php
+                                                if (isset($_POST['subAccepted'])) {
+                                                    $idCandidat = security($_POST['idCandidat']);
+                                                    if ($idCandidat) {
+                                                        $reqVerifExist = $bdd->query("SELECT COUNT(*) AS countid FROM reponsesannonces WHERE candidat='$idCandidat' AND idAnnoncesEntreprises='$idAnnonce'");
+                                                        $reqVerifExistFetch = $reqVerifExist->fetch();
+                                                        if ($reqVerifExistFetch['countid'] != 0) {
+                                                            
+                                                            $getCandidatMail = getIdUser($bdd, $idCandidat);
+                                                            while ($dataCandidatMail = $getCandidatMail->fetch()) {
+                                                                // envoie du mail
+                                                                $sujet = 'Candidature Alt\'itude';
+                                                                $message = "Bonjour ".$dataCandidatMail['prenom']." ".$dataCandidatMail['nom']."<br /><br />";
+                                                                $message .= "Nous avons le plaisir de vous annoncez que la candidature ci-dessous à été acceptée !<br /><br />";
+                                                                $message .= "<b>".$dataAnnonce['titre']."</b> chez <b>".$dataEntreprise['nom']."</b><br /><br />";
+                                                                $message .= "Vous serrez prochainement contacter par le responsable de l'annonce, si vous n'avez toujours de nouvelles n'hésitez à contacter le responsable directement.<br /><br />";
+                                                                $message .= "<i>Si vous n'avez toujours pas de nouvelles contacter nous !</i><br /><br />";
+                                                                $message .= "Bonne continuation,<br /><br />";
+                                                                $message .= "Cordialement, l'équipe Alt'itude";
+                                                                $destinataire = $dataCandidatMail['email'];
+                                                                $headers = "From: \"expediteur moi\"<contact@altitude.maximelefebvre.fr>\n";
+                                                                $headers .= "Reply-To: contact@altitude.maximelefebvre.fr\n";
+                                                                $headers .= "Content-Type: text/html; charset=\"iso-8859-1\"";
+                                                                if(mail($destinataire, $sujet, $message, $headers)) {
+                                                                    echo "<br /><div class='alertSuccess'>Un email a été envoyé au candidat !</div><br />";
+                                                                } else {
+                                                                    echo "<br /><div class='alertError'>Une erreur est survenue (email) !</div><br />";
+                                                                }
+
+                                                                // update de l'état de la candidature
+                                                                $updateCandidature = $bdd->exec("UPDATE reponsesannonces SET reponse='accepte' WHERE candidat='$idCandidat' AND idAnnoncesEntreprises='$idAnnonce'");
+                                                                echo "<div class='alertSuccess'>La candidature a été acceptée !</div><br />";
+                                                            }
+
+                                                        } else echo "<div class='alertError width_50 marginAuto'>Une erreur est survenue !</div><br />";
+                                                    } else echo "<div class='alertError width_50 marginAuto'>Une erreur est survenue !</div><br />";
+                                                }
+
+                                                if (isset($_POST['subRefused'])) {
+                                                    $idCandidat = security($_POST['idCandidat']);
+                                                    if ($idCandidat) {
+                                                        $reqVerifExist = $bdd->query("SELECT COUNT(*) AS countid FROM reponsesannonces WHERE candidat='$idCandidat' AND idAnnoncesEntreprises='$idAnnonce'");
+                                                        $reqVerifExistFetch = $reqVerifExist->fetch();
+                                                        if ($reqVerifExistFetch['countid'] != 0) {
+                                                            
+                                                            $getCandidatMail = getIdUser($bdd, $idCandidat);
+                                                            while ($dataCandidatMail = $getCandidatMail->fetch()) {
+                                                                // envoie du mail
+                                                                $sujet = 'Candidature Alt\'itude';
+                                                                $message = "Bonjour ".$dataCandidatMail['prenom']." ".$dataCandidatMail['nom']."<br /><br />";
+                                                                $message .= "Nous sommes désolé de vous annoncez que la candidature ci-dessous à été refusée !<br /><br />";
+                                                                $message .= "<b>".$dataAnnonce['titre']."</b> chez <b>".$dataEntreprise['nom']."</b><br /><br />";
+                                                                $message .= "Nous espérons que vous aurez prochainement une réponse positive, <b>ne lâchez rien</b> !";
+                                                                $message .= "Bonne continuation,<br /><br />";
+                                                                $message .= "Cordialement, l'équipe Alt'itude";
+                                                                $destinataire = $dataCandidatMail['email'];
+                                                                $headers = "From: \"expediteur moi\"<contact@altitude.maximelefebvre.fr>\n";
+                                                                $headers .= "Reply-To: contact@altitude.maximelefebvre.fr\n";
+                                                                $headers .= "Content-Type: text/html; charset=\"iso-8859-1\"";
+                                                                if(mail($destinataire, $sujet, $message, $headers)) {
+                                                                    echo "<br /><div class='alertSuccess'>Un email a été envoyé au candidat !</div><br />";
+                                                                } else {
+                                                                    echo "<br /><div class='alertError'>Une erreur est survenue (email) !</div><br />";
+                                                                }
+
+                                                                // update de l'état de la candidature
+                                                                $updateCandidature = $bdd->exec("UPDATE reponsesannonces SET reponse='refuse' WHERE candidat='$idCandidat' AND idAnnoncesEntreprises='$idAnnonce'");
+                                                                echo "<div class='alertSuccess'>La candidature a été refusée !</div><br />";
+                                                            }
+
+                                                        } else echo "<div class='alertError width_50 marginAuto'>Une erreur est survenue !</div><br />";
+                                                    } else echo "<div class='alertError width_50 marginAuto'>Une erreur est survenue !</div><br />";
+                                                }
+                                            ?>
+
+                                            <?php
                                                 $getReponse = getRepAnnonceId($bdd, $idAnnonce);
                                                 while ($dataReponse = $getReponse->fetch()){
                                                     $getCandidat = getIdUser($bdd, $dataReponse['candidat']);
@@ -57,11 +132,19 @@
                                                                     <a href="/user/<?php echo $dataCandidat['id']; ?>" target="_blank">Voir le profil</a>
                                                                 </div>
                                                                 <div class="action">
-                                                                    <form action="" method="post">
-                                                                        <input type="text" class="" name="idCandidat" value="<?php echo $dataCandidat['id']; ?>" readonly style="display:none;" />
-                                                                        <input type="submit" name="subAccepted" class="btnAccepted width_30" value="Accepter" style="margin-right: 20px;" />
-                                                                        <input type="submit" name="subRefused" class="btnRefused width_30" value="Refuser" />
-                                                                    </form>
+                                                                <?php
+                                                                    if ($dataReponse['reponse'] != "attente") {
+                                                                        echo "<b>Vous avez déjà accepté ou refusé la candidature !</b>";
+                                                                    } else {
+                                                                        ?>
+                                                                            <form action="" method="post">
+                                                                                <input type="text" name="idCandidat" value="<?php echo $dataCandidat['id']; ?>" readonly style="display:none;" />
+                                                                                <input type="submit" name="subAccepted" class="btnAccepted width_30" value="Accepter" style="margin-right: 20px;" />
+                                                                                <input type="submit" name="subRefused" class="btnRefused width_30" value="Refuser" />
+                                                                            </form>
+                                                                        <?php
+                                                                    }
+                                                                ?>
                                                                 </div>
                                                             </div>
 
@@ -72,7 +155,7 @@
 
                                         <?php
                                     } else {
-                                        echo "erreur";
+                                        echo "<div class='alertError width_50 marginAuto'>Une erreur est survenue !</div><br />";
                                     }
                                 }
 
